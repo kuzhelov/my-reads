fc67fdfd3e1446ab55da22eedcdcdb316cbc6068
Roman Kuzhelev <kuzhelov@Romans-MacBook-Pro.local>
2018-05-14 02:10:11 +0300

    track current book's shelf as state value to rerender on changes

--- src/components/books/index.js
+++ src/components/books/index.js
@@ -9,8 +9,14 @@ import React from 'react'
 // shelfId?: string
 // onChange: (newShelfId: string) => void
 export class Book extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { shelfId: this.props.shelfId };
+    }
+
     handleChange(e) {
         this.props.onChange && this.props.onChange(e.target.value);
+        this.setState({ shelfId: e.target.value });
     }
 
 	render() {
@@ -19,7 +25,7 @@ export class Book extends React.Component {
 			<div className="book-top">
 				<div className="book-cover" style={{ width: 128, height: this.props.cover.height, backgroundImage: `url("${this.props.cover.url}")` }}></div>
 				<div className="book-shelf-changer">
-					<select defaultValue={ this.props.shelfId || '' } onChange={ this.handleChange.bind(this) }>
+					<select value={ this.state.shelfId || '' } onChange={ this.handleChange.bind(this) }>
 						<option value="moveTo" disabled>Move to...</option>
 						<option value="currentlyReading">Currently Reading</option>
 						<option value="wantToRead">Want to Read</option>
